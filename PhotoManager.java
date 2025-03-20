9bd5c9fbef79a5b5ae45509b0e3c7af3efe702f4
anfalaugust <Anfaluv@gmail.com>
2025-03-21 00:17:09 +0300

    Update PhotoManager.java

--- PhotoManager.java
+++ PhotoManager.java
@@ -1,5 +1,21 @@
 package DSproject;
 
-public class PhotoManager {
 
+public class PhotoManager {
+// Constructor
+public PhotoManager() {
+	
+}
+// Return all managed photos
+public LinkedList<Photo> getPhotos(){
+	
+}
+// Add a photo
+public void addPhoto(Photo p) {
+	
+}
+// Delete a photo
+public void deletePhoto(String path) {
+	
+}
 }
