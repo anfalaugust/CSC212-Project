4cd2206838c6c365dc015894cbccd77e4509ac62
Malak-DS <bus.uni.malak@gmail.com>
2025-03-24 00:32:01 +0300

    Update PhotoManager.java

--- PhotoManager.java
+++ PhotoManager.java
@@ -2,9 +2,10 @@ package DSproject;
 
 
 public class PhotoManager {
+   LinkedList<Photo> photos; 
 // Constructor
 public PhotoManager() {
-	
+   photos = new LinkedList<Photo>; 
 }
 // Return all managed photos
 public LinkedList<Photo> getPhotos(){
@@ -17,5 +18,9 @@ public void addPhoto(Photo p) {
 // Delete a photo
 public void deletePhoto(String path) {
 	
+}
+
+public LinkedList<Photo> getPhotos(){
+   return photos;
 }
 }
