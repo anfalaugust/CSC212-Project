d34d63d85a6005245c38d7e4abcf5e78396a765a
anfalaugust <Anfaluv@gmail.com>
2025-04-26 14:05:33 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,11 +1,17 @@
 package DSproject;
 
-public class Node <T> {
+public class Node<T> {
 public T data;
 public Node<T> next;
-  
+	
 public Node (T val) {
 data = val;
 next = null;
 }
+	
+public Node () {
+data = null;
+next = null;
+}
+
 }
